3b785aef9ed386e79acc89d9176c6347072ad68f
min-bok <leehy0782@gmail.com>
2023-05-15 10:59:57 +0900

    thread_list

--- bot.py
+++ bot.py
@@ -6,10 +6,12 @@ dotenv.load_dotenv()
 client = discord.Client(intents=discord.Intents.default())
 token = os.environ.get("MY_TOKEN")
 
-이혜영 = 0
-전유진 = 0
-백경현 = 0
 
+# 목표
+# thread_list = [["스레드 이름", "스레드 생성 날짜", {이름: 메시지수}], ["스레드 이름", "스레드 생성 날짜", {이름: 메시지수}]....]
+# 를 엑셀로 출력
+
+thread_list = []
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game("이달의 오르미 선발"))
@@ -21,13 +23,28 @@ async def on_ready():
         threads = channel.threads
 
         for th in threads:
-            print(th) # 스레드 이름
-            print(str(th.archive_timestamp)[0:10]) # 스레드 생성 날짜
+            name_list = []
+            name_dict = {}
+            total_list = []
+            total_list.append(th) # 스레드 이름
+            total_list.append(str(th.archive_timestamp)[0:10]) # 스레드 생성 날짜
             cnt = 0
             async for msg in th.history(limit=200):
-                name = msg.author.name
-                print(name) # 스레드에 메시지 올린 유저 이름
+                name = msg.author.name # 스레드에 메시지 올린 유저 이름
+                name_list.append(name)
                 cnt += 1
-        
+            for key in name_list:
+                name_dict[key] = name_dict.get(key, 0) + 1
+
+            total_list.append(name_dict)
+            thread_list.append(total_list)
+
+    # print(thread_list[0][0])
+    # print(thread_list[0][1])
+    # print(thread_list[0][2])
+
+    # print(thread_list[1][0])
+    # print(thread_list[1][1])
+    # print(thread_list[1][2])
 
 client.run(token)
